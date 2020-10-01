330cee6a3bab7af82dfdc6ef8ae1ebc37702ad35
Stephan Häuslschmid <stephan.haeuslschmid@gmail.com>
2020-10-01 08:15:35 +0200

    removed error message when connection down to get better logfile

--- monitor.py
+++ monitor.py
@@ -6,7 +6,6 @@ def internet(host="8.8.8.8", port=53, timeout=3):
 		socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((host, port))
 		return True
 	except socket.error as ex:
-		print(ex)
 		return False
 
 def statusChange(newstate):
